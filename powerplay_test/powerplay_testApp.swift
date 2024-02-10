ce8482f814c1a47471cdd8cce625a1696d08f06c
Vitali Yaremchuk <vitaliiyaremchuk.work@gmail.com>
2024-02-10 13:57:11 +0200

    Initial Commit

---
+++ powerplay_test/powerplay_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  powerplay_testApp.swift
+//  powerplay_test
+//
+//  Created by Monty Banks on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct powerplay_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
